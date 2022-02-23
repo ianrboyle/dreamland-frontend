df714601af4a8e5ac91be94ddd5a94c32fb2db2d
Ian Boyle <irboyle21@gmail.com>
2022-02-23 09:15:58 -0500

    changes auth file

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,19 +1,5 @@
 import React from "react"
 
-// const AuthContext = React.createContext(null);
-// interface UserData {
-//   userData: object,
-//   children: any
-// }
-
-// export const AuthProvider: React.FC<UserData> = ({ userData, children }) => {
-//   let [user, setUser] = React.useState(userData);
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => React.useContext(AuthContext);
+const AuthService = () => {
+  
+}
