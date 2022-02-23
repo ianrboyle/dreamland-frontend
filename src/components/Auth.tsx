dc6461442a8d07bacfc3be0e94a847dfbb03df86
Ian Boyle <irboyle21@gmail.com>
2022-02-23 09:32:48 -0500

    installs axios

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,5 +1,15 @@
 import React from "react"
 
-const AuthService = () => {
-  
+const API_URL = "http://localhost:5000/sessions"
+class AuthService {
+  login(email: any, password: any) {
+    return fetch(API_URL, {
+      email,
+      password
+    }).then(response => {
+      if (response?.data.accessToken) {
+        localStorage.set("email", JSON.stringify(response.data))
+      } 
+    })
+  }
 }
