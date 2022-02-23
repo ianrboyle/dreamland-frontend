d20b12dcd663ed48350f008de7669e1fef30d805
Ian Boyle <irboyle21@gmail.com>
2022-02-23 15:27:41 -0500

    adds login component and auth services

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import Register from './components/Register';
-
+import Login from './components/Login';
 function App() {
 
   return (
     <div className="App">
       <Register />
+      <Login />
     </div>
   )
 }
