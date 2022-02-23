e499c215bfd0b8670bc0fdd60d24fcb62a127478
Ian Boyle <irboyle21@gmail.com>
2022-02-22 20:48:50 -0500

    adds candidate checkboxes

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -8,18 +8,18 @@ const LoginButton = () => {
     email: string,
     password: string,
     passwordConfirmation: string,
-    candidate: boolean
   }
   const [userParams, setUserParams] = useState<User | undefined>()
   const [isClicked, setIsClicked] = useState(false)
   
-  
+  const [isCandidate, setIsCandidate] = useState(false)
   const createUser = async (e: any) => {
     e.preventDefault()
     try {
       if (userParams){
         const { name, email, password, passwordConfirmation } = userParams
-        const body = {name, email, password, passwordConfirmation}
+        const candidate = isCandidate
+        const body = {name, email, password, passwordConfirmation, candidate}
       const response =  await fetch("http://localhost:5000/users", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(body) 
     })
     console.log(response, body)
@@ -31,27 +31,50 @@ const LoginButton = () => {
     }
   }
   
-  const handleClick = (e:any) => {
-    e.preventDefault();
-    setIsClicked(!isClicked)
-  }
+
   const handleUserParamsChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const {name, value} = e.target
     setUserParams((prev: any) => {
-      console.log(typeof prev)
       return {...prev, [name]: value}
     })
- 
   }
-  console.log(isClicked)
+
+  const handleCandidateCheckBox = (e:ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.name === "candidate"){
+      setIsCandidate(true)
+    }
+    if (e.target.name === "recruiter"){
+      setIsCandidate(false)
+    }
+   
+  }
+
+  
+  console.log(isCandidate)
   return (
     <div>
-      {isClicked ? <p>IS CLICKED!</p> : <p>NOT CLICKED!</p>}
-      <p>Name</p>: <input type="text" name="name"value={userParams?.name} onChange={handleUserParamsChange}/>
-      <p>Email</p>: <input type="text" name="email"value={userParams?.email} onChange={handleUserParamsChange}/>
-      <p>Password</p>: <input type="text" name="password" value={userParams?.password} onChange={handleUserParamsChange}/>
-      <p>Password Confirmation</p>: <input type="text" name="passwordConfirmation" value={userParams?.passwordConfirmation} onChange={handleUserParamsChange}/>
+      <p>Name: </p> 
+      <input type="text" name="name"value={userParams?.name} onChange={handleUserParamsChange}/>
+
+      <p>Email: </p> 
+      <input type="text" name="email"value={userParams?.email} onChange={handleUserParamsChange}/>
+
+      <p>Password: </p> 
+      <input type="text" name="password" value={userParams?.password} onChange={handleUserParamsChange}/>
+
+      <p>Password Confirmation:</p> 
+      <input type="text" name="passwordConfirmation" value={userParams?.passwordConfirmation} onChange={handleUserParamsChange}/>
+      <div> 
+      <h3>Are you a: </h3>
+      <h3>Candidate? <input defaultChecked={isCandidate} type="checkbox" name="candidate" onChange={handleCandidateCheckBox}/></h3>
+  
+      <h3>or Recruiter? <input type="checkbox" name="recruiter" onChange={handleCandidateCheckBox}/></h3> 
+      
+      </div>
+     
+<br></br>
       <button onClick={createUser}>
         Register
       </button>
