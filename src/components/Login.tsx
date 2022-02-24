f09e733856a5e68e0209ba4bfe1e6cd412da769d
Ian Boyle <irboyle21@gmail.com>
2022-02-24 13:08:33 -0500

    adds method to user login that gets jwt

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, useState} from "react"
-
+import axios from "axios"
 import AuthService from "../services/Auth.service";
+import { response } from "express";
 
 type UserLoginInfo = {
   email: string,
@@ -17,22 +18,50 @@ const Login = () => {
     })
   }
 
-  const handleLogin = () => {
-    AuthService.login(userInfo?.email, userInfo?.password).then(
-      () => {
-        window.location.reload();
-      },error => {
-        const resMessage =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-          console.log(resMessage)
-        });
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    // AuthService.login(userInfo?.email, userInfo?.password).then(
+    //   () => {
+    //     window.location.reload();
+    //   },error => {
+    //     const resMessage =
+    //       (error.response &&
+    //         error.response.data &&
+    //         error.response.data.message) ||
+    //       error.message ||
+    //       error.toString();
+    //       console.log(resMessage)
+    //     });
+
+    axios.post("http://localhost:5000/sessions", userInfo).then((response) => {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+  localStorage.setItem("jwt", response.data.jwt);
+    console.log(response.data)
+}).catch((err) => {
+    console.log(err.response)
+  })
+        // try {
+        //   if (userInfo){
+        //     const {  email, password } = userInfo
+            
+        
+        //     const body = { email, password }
+            // const response =  await fetch("http://localhost:5000/sessions", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(body) 
+
+          
+        // })
+        // console.log(response)
+        // const user: any = JSON.parse(localStorage.getItem('user')!);
+        // console.log(user)
+        // window.location.href = "/"
+          // }
+          
+        // } catch (err: any) {
+        //   console.error(err.message)
+        // }
+
       }
-  
-  console.log(userInfo)
+
   return (  <div>   
     <h1>Login</h1>
      <p>Email: </p> 
--- src/services/AuthHeader.tsx
+++ src/services/AuthHeader.tsx
@@ -1,9 +1,17 @@
+import axios from "axios"
+
 export default function authHeader(): any {
-  const user: any = JSON.parse(localStorage.getItem('email')!);
-  if (user && user.jwt) {
-    return { Authorization: 'Bearer ' + user.jwt };
-  } else {
-    return {};
-  }
+  var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
 }
+  // const user: any = JSON.parse(localStorage.getItem('jwt')!);
+  // if (user && user.jwt) {
+  //   return { Authorization: 'Bearer ' + user.jwt };
+  // } else {
+  //   return {};
+  // }
+}
+
+
 
